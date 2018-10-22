e35617a12b214914ae20b787138937e9880fed3f
Leandro <leandro.l2r@gmail.com>
2018-10-22 20:55:04 -0300

    Add dependencies installation on Dockerfile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 while [ true ]; do
-    automatic_summarizer migrate --noinput
+    python manage.py migrate --noinput
     if [ $? -eq 0 ]; then
         break
     fi
     sleep 5
 done
 
-echo "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@automaticsummarizer', 'abc123')" | automatic_summarizer shell
-automatic_summarizer runserver 0.0.0.0:8080
+echo "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@automaticsummarizer', 'abc123')" | python manage.py shell
+python manage.py runserver 0.0.0.0:8080
